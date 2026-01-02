2a617880f19964dccf7aeea3d598be1aea76330a
Ian Farai Madhara <iannyfarai@gmail.com>
2026-01-01 20:29:08 -0500

    added gateway

---
+++ moirai-gateway/src/main/java/com/hexicode/moirai_gateway/MoiraiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hexicode.moirai_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiraiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiraiGatewayApplication.class, args);
+	}
+
+}
---
+++ moirai-gateway/src/test/java/com/hexicode/moirai_gateway/MoiraiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexicode.moirai_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiraiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
